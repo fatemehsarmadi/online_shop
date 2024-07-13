ae325af9439076a24bdd7555cb01249535d4dffd
fatemehsarmadi <Fatemeh.Sarmady@gmail.com>
2024-07-13 17:02:01 +0330

    fix: resolve bugs in product model
    Fixed an issue in the product model related to rating field. Corrected the max digits issue

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class AbstractProductModel(models.Model):
     title = models.CharField(max_length=255)
     price = models.PositiveIntegerField()
-    rating = models.DecimalField(max_digits=1, decimal_places=1)
+    rating = models.DecimalField(max_digits=5, decimal_places=1)
 
     class Meta:
         abstract = True
