1301dd9b61c6c3a2e0ea55c5c3915ee48611b95e
fatemehsarmadi <Fatemeh.Sarmady@gmail.com>
2024-07-13 17:45:14 +0330

    feat: create view to retrieve all mobile data

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+from . import models
 
-# Create your views here.
+def all_mobiles(request: HttpRequest):
+    if request.method == 'GET':
+        mobiles = list(models.Mobile.objects.all().values())
+        return JsonResponse(mobiles, safe=False)
+    return JsonResponse({'error': 'Please use GET method to get all mobiles.'})
