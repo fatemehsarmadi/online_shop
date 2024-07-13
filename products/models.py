d3e8cac62e2feceb1e6f12d1f936bbf000753588
fatemehsarmadi <Fatemeh.Sarmady@gmail.com>
2024-07-13 18:15:42 +0330

    fix: set default value for rating field of product model

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class AbstractProductModel(models.Model):
     title = models.CharField(max_length=255)
     price = models.PositiveIntegerField()
-    rating = models.DecimalField(max_digits=5, decimal_places=1)
+    rating = models.DecimalField(max_digits=5, decimal_places=1, default=0)
 
     class Meta:
         abstract = True
