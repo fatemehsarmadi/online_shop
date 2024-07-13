b98a2cde9f5af109b8a9144f69f58230f2593b79
fatemehsarmadi <Fatemeh.Sarmady@gmail.com>
2024-07-13 13:18:59 +0330

    chore: update model name from Product to AbstractProductModel

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Product(models.Model):
+class AbstractProductModel(models.Model):
     title = models.CharField(max_length=255)
     price = models.PositiveIntegerField()
     rating = models.DecimalField(max_digits=1, decimal_places=1)
