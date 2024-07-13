f880bf25406bef976aca614651d1f8617fa3a649
fatemehsarmadi <Fatemeh.Sarmady@gmail.com>
2024-07-13 18:03:41 +0330

    chore: add URL pattern for the new endpoint for get data of a mobile

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'products'
 urlpatterns = [
     path('all_mobiles/', views.all_mobiles, name='all_mobiles'),
+    re_path(r'^mobile/(?P<id>\d+)/$', views.get_mobile),
 ]
